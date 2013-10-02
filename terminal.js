7db49c0feed4ba61b3870ca6007523040faa71f1
ioanaculic <ioana_mculic@yahoo.com>
2013-10-02 18:46:55 +0000

    terminal

--- terminal.js
+++ terminal.js
@@ -8,7 +8,8 @@ var terminals=[];
 function alloc_terminal()
 {
 	var id = find_terminal_id();
-	var t = {id:id};
+	var t = {id:id,
+				terminal:null};
 	terminals[id] = t;
 	return t;
 }
@@ -34,8 +35,30 @@ function find_terminal_id()
 	}
 }
 
-function delete_terminal(id)
+function find_terminal_by_id(id)
 {
+	var i=0;
+	while(i<terminals.length)
+	{
+		if(terminals[i].id == id)
+			return terminals[i];
+		i++;
+	}
+	//eroare; nu gaseste terminalul; ce fac????
+}
+
+function destroy_terminal(id)
+{
+	var t = find_terminal_by_id(id);
+	//verific daca exista!!!!
+
+	//verific daca s-a facut terminalul sau doar s-a alocat
+	if(t.terminal != null)
+	{
+		console.log("not null");
+		t.terminal.kill();
+	}
+	console.log("destroy");
 	terminals[id] = null;
 }
 
@@ -48,20 +71,17 @@ function start_terminal(id, command)
 	  cwd: process.env.HOME,
 	  env: process.env
 	});
+	term.on('data', function(data) {
+  console.log(data);
+});
 }
 
 init_terminals();
 console.log(alloc_terminal(find_terminal_id()).id);
 console.log(alloc_terminal(find_terminal_id()).id);
-delete_terminal(1);
+destroy_terminal(1);
 console.log(alloc_terminal(find_terminal_id()).id);
 console.log(alloc_terminal(find_terminal_id()).id);
-var term = pty.spawn('vi', [], {
-	  name: 'xterm-color',
-	  cols: TERMINAL_COLS,
-	  rows: TERMINAL_ROWS,
-	  cwd: process.env.HOME,
-	  env: process.env
-	});
-//start_terminal(1,'vi');
-
+start_terminal(1,'vi');
+setTimeout(destroy_terminal(1), 3000);
+console.log("end");
