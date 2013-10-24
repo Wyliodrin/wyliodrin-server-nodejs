696d4832b7804b02fca0695104f430d788a8de67
Alexandru Radovici <msg4alex@gmail.com>
2013-10-24 15:07:09 +0300

    Update files.js

--- files.js
+++ files.js
@@ -340,21 +340,21 @@ function open(path, flags, cb) {
 //  * cb: a callback of the form cb(err, stat), where err is the Posix return code
 //  *     and stat is the result in the form of a statvfs structure (when err === 0)
 //  */
-// function statfs(cb) {
-//   cb(0, {
-//       bsize: 1000000,
-//       frsize: 1000000,
-//       blocks: 1000000,
-//       bfree: 1000000,
-//       bavail: 1000000,
-//       files: 1000000,
-//       ffree: 1000000,
-//       favail: 1000000,
-//       fsid: 1000000,
-//       flag: 1000000,
-//       namemax: 1000000
-//   });
-// }
+function statfs(cb) {
+  cb(0, {
+      bsize: 1000000,
+      frsize: 1000000,
+      blocks: 1000000,
+      bfree: 1000000,
+      bavail: 1000000,
+      files: 1000000,
+      ffree: 1000000,
+      favail: 1000000,
+      fsid: 1000000,
+      flag: 1000000,
+      namemax: 1000000
+  });
+}
 
 // //---------------------------------------------------------------------------
 
