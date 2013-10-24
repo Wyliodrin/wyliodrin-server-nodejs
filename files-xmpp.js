7bd01cb6bbd780fc7c72879c12455ebf602efba1
Alexandru Radovici <msg4alex@gmail.com>
2013-10-24 15:06:49 +0300

    Update files-xmpp.js

--- files-xmpp.js
+++ files-xmpp.js
@@ -132,26 +132,9 @@ function open(path, sendResult)
 	}
 }
 
-function statfs(cb) {
-  cb(0, {
-      bsize: 1000000,
-      frsize: 1000000,
-      blocks: 1000000,
-      bfree: 1000000,
-      bavail: 1000000,
-      files: 1000000,
-      ffree: 1000000,
-      favail: 1000000,
-      fsid: 1000000,
-      flag: 1000000,
-      namemax: 1000000
-  });
-}
-
 exports.load = load;
 exports.loadConfig = loadConfig;
 exports.files_stanza = files_stanza;
 exports.readDir = readDir;
 exports.getAttr = getAttr;
-exports.statfs = statfs;
 exports.open = open;
