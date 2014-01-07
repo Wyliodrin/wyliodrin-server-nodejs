3c0ab5078a428f3e24d22519ced3429a267761e6
Alexandru Radovici <msg4alx@gmail.com>
2014-01-07 21:52:32 +0000

    upstart

--- wyliodrin-server-raspberry-pi.sh
+++ wyliodrin-server-raspberry-pi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # raspberry pi user
 USER=pi
@@ -33,6 +33,35 @@ git clone https://github.com/Gadgetoid/WiringPi2-Python.git
 cd WiringPi2-Python
 sudo python setup.py install
 cd ..
+
+# installing upstart
+echo Wyliodrin uses upstart for starting the service
+echo This will replace system init
+echo Please be sure that you know what you are doing
+echo If you do not install upstart, Wyliodrin will not start
+echo automatically
+if sudo apt-get install upstart
+then
+
+echo "
+# wyliodrin server
+
+description \"wyliodrin server\"
+author \"Ioana Culic\"
+
+start on runlevel [2345]
+stop on runlevel [016]
+chdir `pwd`
+script
+        export NODE_PATH=\"/usr/local/lib/node_modules\"
+        sudo -E -u $USER /usr/local/bin/npm start
+end script
+respawn
+" > wyliodrin-server.conf
+
+sudo cp wyliodrin-server.conf /etc/init/wyliodrin.conf
+
+fi
 
 echo You need to logout and goin again
 
