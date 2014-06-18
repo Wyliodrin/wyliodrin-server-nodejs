9761a3cc3c271ddd3a6a5466d6b542e2f4b86f41
Alexandru Radovici <msg4alex@gmail.com>
2014-06-18 19:16:51 +0300

    Update arduinogalileo_wifi.sh

--- conf/arduinogalileo_wifi.sh
+++ conf/arduinogalileo_wifi.sh
@@ -5,19 +5,20 @@ connmanctl enable wifi
 sleep 1
 connmanctl scan wifi
 WIFIHASH=`connmanctl services | grep "$1" | awk '{print $NF}'`
-mkdir -p /var/lib/connman/$WIFIHASH
-WIFICONFIG=/var/lib/connman/$WIFIHASH/settings
-echo "[$WIFIHASH]" > $WIFICONFIG
-echo "  Name=$1" >> $WIFICONFIG
-echo "  SSID=`echo $WIFIHASH | cut -d \"_\" -f 3`" >> $WIFICONFIG
-echo "  Favorite=True" >> $WIFICONFIG
-echo "  Passphrase=$2" >> $WIFICONFIG
-echo "  IPv4.method=dhcp" >> $WIFICONFIG
-echo "  IPv6.method=auto" >> $WIFICONFIG
-echo "  IPv6.privacy=disabled" >> $WIFICONFIG
-connmanctl disable wifi
-connmanctl enable wifi
-sleep 1
-connmanctl scan wifi
-connmanctl connect $WIFIHASH
-
+if test -n $WIFIHASH; then
+  mkdir -p /var/lib/connman/$WIFIHASH
+  WIFICONFIG=/var/lib/connman/$WIFIHASH/settings
+  echo "[$WIFIHASH]" > $WIFICONFIG
+  echo "  Name=$1" >> $WIFICONFIG
+  echo "  SSID=`echo $WIFIHASH | cut -d \"_\" -f 3`" >> $WIFICONFIG
+  echo "  Favorite=True" >> $WIFICONFIG
+  echo "  Passphrase=$2" >> $WIFICONFIG
+  echo "  IPv4.method=dhcp" >> $WIFICONFIG
+  echo "  IPv6.method=auto" >> $WIFICONFIG
+  echo "  IPv6.privacy=disabled" >> $WIFICONFIG
+  connmanctl disable wifi
+  connmanctl enable wifi
+  sleep 1
+  connmanctl scan wifi
+  connmanctl connect $WIFIHASH
+fi
