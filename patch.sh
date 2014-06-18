771de581df2227b8a68b6d15b977eff13d765603
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2014-06-18 05:04:11 +0300

    version 1 websockets

--- patch.sh
+++ patch.sh
@@ -2,4 +2,5 @@
 
 cp patch/index.js node_modules/node-xmpp-client/index.js
 cp patch/session.js node_modules/node-xmpp-client/lib/session.js
+cp patch/websockets.js node_modules/node-xmpp-client/lib/websockets.js
 
