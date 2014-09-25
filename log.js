0b087322556a4d9c6f8d20dfbeee1349d3962d19
Alexandru Radovici <msg4alex@gmail.com>
2014-09-25 17:01:28 +0300

    Update log.js

--- log.js
+++ log.js
@@ -1,26 +1,90 @@
 "use strict";
 
-var wxmpp = require('./wxmpp');
-var xmpp = require('./xmpp_library.js').xmpp;
+// var wxmpp = require('./wxmpp');
+// var xmpp = require('./xmpp_library.js').xmpp;
 var logs = [];
-var config = require('./settings').config.networkConfig;
+// var config = require('./settings').config.networkConfig;
 
-function flush ()
+// function flush ()
+// {
+// 	while (wxmpp && wxmpp.checkConnected && wxmpp.checkConnected() && logs.length > 0)
+// 	{
+// 		var log = logs[0];
+// 		logs.splice (0,1);
+// 		var connection = wxmpp.getConnection ();
+// 		var tag = new xmpp.Element('log', {type:logs.type, timestamp:log.timestamp}).t (new Buffer (log.str).toString('base64'));
+// 		connection.sendWyliodrin (config.owner, tag);
+// 	}
+// }
+
+var https = require('https');
+var settings = require('./settings').config;
+var networkConfig = settings.networkConfig;
+
+var domain;
+
+function load()
 {
-	while (wxmpp && wxmpp.checkConnected && wxmpp.checkConnected() && logs.length > 0)
+	var arr = networkConfig.jid.split("@");
+	for(var i=0; i<arr.length; i++)
 	{
-		var log = logs[0];
-		logs.splice (0,1);
-		var connection = wxmpp.getConnection ();
-		var tag = new xmpp.Element('log', {type:logs.type, timestamp:log.timestamp}).t (new Buffer (log.str).toString('base64'));
-		connection.sendWyliodrin (config.owner, tag);
+		domain = arr[i];
 	}
+	setInterval (function ()
+	{
+		sendLogs ();
+	}, 3000);
+}
+
+function sendLogs()
+{
+	if (!domain) load ();
+	var s = {str:logs.join ('\n')};
+	var options =
+	{
+	  hostname: domain,
+	  port: 443,
+	  path: '/gadgets/logs/'+networkConfig.jid,
+	  method: 'POST',
+	  headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': s.length,
+          'Connection': 'close'
+      }
+	};
+
+	var req = https.request(options, function(res) {
+		//console.log("codul de eroare :"+res.statusCode);
+	  // functie(false, res.statusCode);
+	  if (res.statusCode == 200)
+	  {
+	  	logs = [];
+	  }
+	});
+
+	req.on('socket', function (socket) {
+	    socket.setTimeout(2000);  
+	    	socket.on('timeout', function() {
+        		//console.log ('socket timeout');
+        		req.abort();
+    		});
+	});
+
+	req.write(s);
+	req.end();
+
+	req.on('error', function(e) {
+	  console.error(e);
+	  // functie (e);
+	});
 }
 
 function send (type, str, timestamp)
 {
-	logs.push ({type:type, str:str, timestamp:timestamp});
-	flush ();
+	// logs.push ({type:type, str:str, timestamp:timestamp});
+	// flush ();
+	if (logs.length > 100000) logs.splice (0, 1000);
+	logs.push (type+' '+timestamp+' '+str);
 }
 
 function putLog(log)
