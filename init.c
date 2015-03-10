98b2697e3f29b3605d6733bcda52b9a1f062ad19
Razvan Madalin MATEI <matei.rm94@gmail.com>
2015-03-10 16:45:05 +0200

    return values of init and nice logs

--- init.c
+++ init.c
@@ -14,28 +14,37 @@
 
 /**
  * Init wyliodrin client
+ *
+ * RETURN
+ *     0 : succes
+ * 		-1 : NULL settings JSON
+ * 		-2 : config_file value is not a string
+ * 		-3 : NULL config JSON
  */
-void init() {
+int8_t init() {
+	wlog("init()");
+
 	/* Decode settings JSON */
 	json_t *settings = decode_json_text(SETTINGS_PATH); /* Settings JSON */
 	if(settings == NULL) {
-		perror("[werr] Decode settings");
-		return;
+		wlog("Return -1 due to NULL decoded settings JSON");
+		return -1;
 	}
 
 	/* Check whether config_file exists or not */
 	json_t *config_file = json_object_get(settings, "config_file"); /* config_file object */
 	if(!json_is_string(config_file)) {
-		perror("[werr] Settings file should contain config_file");
 		json_decref(settings);
-		return;
+
+		wlog("Return -2 because config_file value is not a string");
+		return -2;
 	}
 	const char *config_file_txt = json_string_value(config_file); /* config_file text */
 	if(strcmp(config_file_txt, "") == 0) {
-		perror("[werr] No config file");
 		json_decref(settings);
 
 		/* Sleep and try again */
+		wlog("Config file value is an empty string. Sleep and retry.");
 		sleep(SLEEP_NO_CONFIG);
 		return init();
 	}
@@ -43,9 +52,10 @@ void init() {
 	/* Decode config JSON */
 	json_t *config = decode_json_text(config_file_txt); /* config_file JSON */
 	if(config == NULL) {
-		perror("[werr] Decode config");
 		json_decref(settings);
-		return;
+
+		wlog("Return -3 due to NULL decoded config JSON");
+		return -3;
 	}
 
 	/* Do stuff */
@@ -54,6 +64,9 @@ void init() {
 	/* Cleaning */
 	json_decref(settings);
 	json_decref(config);
+
+	wlog("Return 0 successful initialization");
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
