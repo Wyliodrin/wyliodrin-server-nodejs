4c80c1a8fc20a64739f4135d0c5ea828f943383b
Ioana Culic <ioana_mculic@yahoo.com>
2015-05-05 11:38:38 +0300

    redpitaya added

--- conf/wyliodrin_settings.js
+++ conf/wyliodrin_settings.js
@@ -13,6 +13,17 @@ var settings =
 		sudo: 'sudo',
 		board:"raspberrypi"
 	},
+	redpitaya:{
+		config_file: '/boot/wyliodrin.json',
+		home: '/wyliodrin',
+		mountFile: '/wyliodrin/projects/mount',
+		buildFile: '/wyliodrin/projects/build',
+		umount: 'umount -f',
+		run: ['make'],
+		stop: ['kill -9'],
+		sudo: '',
+		board:"redpitaya"
+	},
 
 	beagleboneblack:{
 		config_file: '/boot/uboot/wyliodrin.json',
