5a64693914ab1b7384c6d36bbef0994d3b556588
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2015-06-16 13:18:12 +0000

    server

--- conf/wyliodrin_settings.js
+++ conf/wyliodrin_settings.js
@@ -13,6 +13,17 @@ var settings =
 		sudo: 'sudo',
 		board:"raspberrypi"
 	},
+	server:{
+		config_file: '/data/wyliodrin.json',
+		home: '/wyliodrin',
+		mountFile: '/wyliodrin/projects/mount',
+		buildFile: '/wyliodrin/projects/build',
+		umount: 'sudo umount -f',
+		run: ['sudo', '-E', 'make'],
+		stop: ['sudo kill -9'],
+		sudo: 'sudo',
+		board:"server"
+	},
 	redpitaya:{
 		config_file: '/boot/wyliodrin.json',
 		home: '/wyliodrin',
